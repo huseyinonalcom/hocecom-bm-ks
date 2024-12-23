c728dc939aa3500bb73042f5e8512fed794efcbf
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2024-12-23 11:12:31 +0300

    d

--- .keystone/config.js
+++ .keystone/config.js
@@ -34,6 +34,61 @@ __export(keystone_exports, {
 });
 module.exports = __toCommonJS(keystone_exports);
 
+// utils/bulkdocumentsenderstart.ts
+var import_worker_threads = require("worker_threads");
+var import_path = __toESM(require("path"));
+async function fetchCompany(companyID, context) {
+  let company;
+  await context.sudo().query.Company.findOne({
+    query: "id name accountantEmail logo { url } emailHost emailPort emailUser emailPassword",
+    where: { id: companyID }
+  }).then((res) => {
+    company = res;
+  });
+  return company;
+}
+async function fetchDocuments(companyID, docTypes, month, year, context) {
+  const fetchedDocuments = await context.sudo().query.Document.findMany({
+    orderBy: [{ date: "desc" }],
+    query: "id number establishment { taxID logo { url } } supplier { name address { street door zip city country } taxId } date files { url name } type products { name amount totalWithTaxAfterReduction tax }",
+    where: {
+      company: {
+        id: {
+          equals: companyID
+        }
+      },
+      type: {
+        in: docTypes
+      },
+      date: {
+        gte: new Date(year, month - 1, 1),
+        lte: new Date(year, month, 1)
+      }
+    }
+  });
+  return Array.from(fetchedDocuments);
+}
+async function startBulkDocumentSenderWorker({ companyID, docTypes, month, year, context }) {
+  const documents = await fetchDocuments(companyID, docTypes, month, year, context);
+  if (documents.length === 0) {
+    console.log("No documents found for companyID: ", companyID, "docTypes: ", docTypes, "month: ", month, "year: ", year);
+    return;
+  }
+  const company = await fetchCompany(companyID, context);
+  const workerData = { documents, company };
+  new import_worker_threads.Worker(import_path.default.resolve("./utils/bulkdocumentsenderworker.ts"), {
+    execArgv: ["-r", "ts-node/register"],
+    workerData
+  });
+}
+var bulkSendDocuments = ({ companyID, docTypes, month, year, context }) => {
+  try {
+    startBulkDocumentSenderWorker({ companyID, docTypes, month, year, context });
+  } catch (error) {
+    console.error("Error occurred while starting bulkdocumentsender with params: ", companyID, docTypes, month, year, "error: ", error);
+  }
+};
+
 // utils/fileupload.ts
 var import_client_s3 = require("@aws-sdk/client-s3");
 var import_config = require("dotenv/config");
@@ -923,7 +978,11 @@ var lists = {
     },
     fields: {
       name: (0, import_fields.text)({ validation: { isRequired: true } }),
-      defaultCurrency: (0, import_fields.text)({ defaultValue: "EUR" }),
+      defaultCurrency: (0, import_fields.select)({
+        type: "string",
+        options: ["TRY", "USD", "EUR"],
+        defaultValue: "EUR"
+      }),
       logo: (0, import_fields.relationship)({
         ref: "File",
         many: false
@@ -1926,63 +1985,6 @@ var lists = {
 
 // keystone.ts
 var import_config2 = require("dotenv/config");
-
-// utils/bulkdocumentsenderstart.ts
-var import_worker_threads = require("worker_threads");
-var import_path = __toESM(require("path"));
-async function fetchCompany(companyID, context) {
-  let company;
-  await context.sudo().query.Company.findOne({
-    query: "id name accountantEmail logo { url } emailHost emailPort emailUser emailPassword",
-    where: { id: companyID }
-  }).then((res) => {
-    company = res;
-  });
-  return company;
-}
-async function fetchDocuments(companyID, docTypes, month, year, context) {
-  const fetchedDocuments = await context.sudo().query.Document.findMany({
-    orderBy: [{ date: "desc" }],
-    query: "id number establishment { taxID logo { url } } supplier { name address { street door zip city country } taxId } date files { url name } type products { name amount totalWithTaxAfterReduction tax }",
-    where: {
-      company: {
-        id: {
-          equals: companyID
-        }
-      },
-      type: {
-        in: docTypes
-      },
-      date: {
-        gte: new Date(year, month - 1, 1),
-        lte: new Date(year, month, 1)
-      }
-    }
-  });
-  return Array.from(fetchedDocuments);
-}
-async function startBulkDocumentSenderWorker({ companyID, docTypes, month, year, context }) {
-  const documents = await fetchDocuments(companyID, docTypes, month, year, context);
-  if (documents.length === 0) {
-    console.log("No documents found for companyID: ", companyID, "docTypes: ", docTypes, "month: ", month, "year: ", year);
-    return;
-  }
-  const company = await fetchCompany(companyID, context);
-  const workerData = { documents, company };
-  new import_worker_threads.Worker(import_path.default.resolve("./utils/bulkdocumentsenderworker.ts"), {
-    execArgv: ["-r", "ts-node/register"],
-    workerData
-  });
-}
-var bulkSendDocuments = ({ companyID, docTypes, month, year, context }) => {
-  try {
-    startBulkDocumentSenderWorker({ companyID, docTypes, month, year, context });
-  } catch (error) {
-    console.error("Error occurred while starting bulkdocumentsender with params: ", companyID, docTypes, month, year, "error: ", error);
-  }
-};
-
-// keystone.ts
 var keystone_default = withAuth(
   (0, import_core2.config)({
     db: {
