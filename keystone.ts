f6c4368f65f4b454666178eaa67fe9d5ff3b2ee2
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-02-19 11:24:31 +0300

    Update keystone.ts

--- keystone.ts
+++ keystone.ts
@@ -20,7 +20,7 @@ export default withAuth(
     server: {
       port: 3400,
       cors: {
-        origin: ["https://dfatest.huseyinonal.com", "http://localhost:3399", "https://acc.digitalforge.be", "https://dfa.huseyinonal.com"],
+        origin: ["https://dfatest.huseyinonal.com", "http://localhost:3399", "https://acc.digitalforge.be", "https://dfa.huseyinonal.com", "https://web.digitalforge.be"],
         credentials: true,
       },
       extendExpressApp: (app, context) => {
