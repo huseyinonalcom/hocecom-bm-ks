b1eb42bd4ce945f49c59b7b9fb8ce4c328705b97
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2025-09-16 01:32:48 +0300

    Update keystone.ts

--- keystone.ts
+++ keystone.ts
@@ -31,6 +31,7 @@ export default withAuth(
           "http://localhost:3454",
           "https://acc.digitalforge.be",
           "https://dfa.huseyinonal.com",
+          "https://bm.huseyinonal.com",
           "https://web.digitalforge.be",
         ],
         credentials: true,
